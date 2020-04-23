a86b1e741e5496995b2958e8d3f9cf53a0ee124e
Bivor Adrito <bivor500@gmail.com>
2020-04-23 16:48:05 +0600

    complete list adding items with for loop ejs

--- app.js
+++ app.js
@@ -6,43 +6,38 @@ const request = require("request");
 
 
 const app = express();
+
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: false}))
  
 app.get('/', function (req, res) {
 
     var today = new Date(); // this is a js Date constructor which i had put in a var to create an object see MDN js Date() docs to learn more;
-    var currentDay = today.getDay();
-     var day = "";
+    var options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
 
-    //Date.getDay is a built in js function which gives the value from 0-6
-    switch (currentDay) {
-        case 0:
-          day = "Sunday";
-          break;
-        case 1:
-          day = "Monday";
-          break;
-        case 2:
-           day = "Tuesday";
-          break;
-        case 3:
-          day = "Wednesday";
-          break;
-        case 4:
-          day = "Thursday";
-          break;
-        case 5:
-          day = "Friday";
-          break;
-        case 6:
-          day = "Saturday";
+    var day = today.toLocaleDateString("en-US", options);
 
-        default:
-            console.log("Error current day is equal to: " + currentDay);
-      }
+   
+    res.render('list', {kindOfDay: day, newListItem: items});
 
-    res.render('list', {kindOfDay: day});
   //res.send('Hello World');
+});
+
+app.post("/", function(req, res){
+    item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+
 });
  
 app.listen(3000, function(){
